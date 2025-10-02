814a98375822c40d292200ffc1e0220afab4f9c6
hagyeong <hagyeong13@naver.com>
2025-10-03 04:22:15 +0900

    Feat : homepage 화면 구성

--- jeonsafe/src/pages/homePage.tsx
+++ jeonsafe/src/pages/homePage.tsx
@@ -1,6 +1,26 @@
+import HomeBox from "../components/homebox";
+
 const HomePage = () => {
     return (
         <div className="w-full">
+            <section className="max-w-[1100px] mx-auto px-6 py-14 mt-16">
+                <h1 className="text-center font-extrabold leading-tight text-[#113F67] text-[40px] md:text-[50px]">
+                    <span className="mr-2">“</span>
+                    전세 계약, 안전하게 할 수 있을까?
+                    <span className="ml-2">”</span>
+                </h1>
+
+                <div className="mt-18 grid grid-cols-1 md:grid-cols-2 gap-12 justify-items-center">
+                    <HomeBox
+                        section="사전 대비"
+                        introduce="전세 계약 전이거나 전세 계약을 생각 중이에요!"
+                    />
+                    <HomeBox
+                        section="사후 처리"
+                        introduce="전세 사기를 당한 것 같아요."
+                    />
+                </div>
+            </section>
         </div>
     )
 }
