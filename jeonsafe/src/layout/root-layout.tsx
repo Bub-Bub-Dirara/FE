f0a6663a5c25efbe7c516c8656061b295f8e4f54
hagyeong <hagyeong13@naver.com>
2025-10-30 14:37:41 +0900

    Feat: collectpage merge

--- jeonsafe/src/layout/root-layout.tsx
+++ jeonsafe/src/layout/root-layout.tsx
@@ -1,11 +1,9 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 
 const RootLayout = () => {
     return (
         <>
             <Navbar />
-            <Outlet />
         </>
     )
 }
